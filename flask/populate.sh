b2044a0c5c4e5146be21c7fa249cc69b0853d011
ranganathanm <mranga@nist.gov>
2014-07-02 17:10:43 -0400

    Added populate shell script for testing.

---
+++ flask/populate.sh
@@ -0,0 +1,6 @@
+ python populate_db.py -data data/bldr/Bld14_1_612Ascii.dat
+ python populate_db.py -data data/gburg/LTE_UL_bc17_ts106_p1.dat 
+ python populate_db.py -data data/gburg/LTE_UL_bc17_ts106_p2.dat 
+ python populate_db.py -data data/gburg/LTE_UL_bc17_ts106_p3.dat 
+ python populate_db.py -data data/gburg/LTE_UL_bc17_ts106_p4.dat 
+ python populate_db.py -data data/gburg/LTE_UL_bc17_ts106_p5.dat 
