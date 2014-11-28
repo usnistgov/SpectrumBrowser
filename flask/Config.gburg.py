6d2b5f9d7d4f09194ffcf1ef59ae8933791eac67
ranganathanm <mranga@nist.gov>
2014-11-28 18:24:55 -0500

    Federated system config hackery.

--- flask/Config.gburg.py
+++ flask/Config.gburg.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import os
+import netifaces
 
 # This is a placeholder. After first system install, we need a page that
 # Expects these things to be entered before the system becomes operational.
@@ -34,6 +35,16 @@ oldConfig = db.configuration.find_one({})
 if oldConfig != None:
     db.configuration.remove(oldConfig)
 
+# Determine my host address by looking at the interfaces
+# and the default route (not always foolproof but works
+# most of the time). If you use specific routes,
+# this will not work.
+gws = netifaces.gateways()
+gw = gws['default'][netifaces.AF_INET]
+addrs = netifaces.ifaddresses(gw[1])
+MY_HOST_NAME = addrs[netifaces.AF_INET][0]['addr']
+
+
 configuration = {"API_KEY":API_KEY,\
 "SMTP_SERVER":SMTP_SERVER, \
 "SMTP_PORT" : SMTP_PORT,\
@@ -46,7 +57,9 @@ configuration = {"API_KEY":API_KEY,\
 "STREAMING_FILTER": STREAMING_FILTER,\
 "STREAMING_SERVER_PORT": STREAMING_SERVER_PORT,\
 "IS_AUTHENTICATION_REQUIRED": IS_AUTHENTICATION_REQUIRED,\
-"PEERS":PEERS}
+"PEERS":PEERS,\
+"HOST_NAME":MY_HOST_NAME
+}
 
 db.configuration.insert(configuration)
 
