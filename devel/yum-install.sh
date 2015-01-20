cdbb15763114a031a9a867e80ad8f35a50427ad1
ranganathanm <mranga@nist.gov>
2015-01-20 10:52:17 -0500

    Added install scripts.

---
+++ devel/yum-install.sh
@@ -0,0 +1,14 @@
+yum group install "C Development Tools and Libraries"
+sudo yum install openssl
+sudo yum install openssl-devel
+sudo yum install libffi
+sudo yum install libffi-devel
+sudo yum install libpbg
+sudo yum install libpng-devel
+sudo yum install freetype
+sudo yum install freetype-devel
+yum install libevent
+yum install libevent-devel
+yum install agg
+yum install memcached 
+yum install gtk2-devel
