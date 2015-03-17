a122764f939d5821f194b3532425cc2327102019
ranganathanm <mranga@nist.gov>
2015-03-17 17:00:29 -0400

    Issue-136: Read environment variable overrides for debug flags. Add start-gunicorn for generating and running tests.

--- flask/authentication.py
+++ flask/authentication.py
@@ -49,7 +49,7 @@ def checkSessionId(sessionId):
         remoteAddress = None
  
     sessionFound = False
-    if DebugFlags.disableSessionIdCheck :
+    if DebugFlags.getDisableSessionIdCheckFlag() :
         sessionFound = True
     else:
         SessionLock.acquire() 
@@ -117,7 +117,7 @@ def generateSessionKey(privilege):
         num = 0
         SessionLock.acquire()
         # try 5 times to get a unique session id
-        if DebugFlags.disableSessionIdCheck:
+        if DebugFlags.getDisableSessionIdCheckFlag():
             return privilege + "-" + str(123)
         else:
             while (not uniqueSessionId) and (num < 5):
