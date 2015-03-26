788055e8a70e3baa6a51a8af923a051545a5672e
ranganathanm <mranga@nist.gov>
2015-03-26 16:56:08 -0400

    Add start of data streaming server.

--- flask/start-gunicorn-generate-testcases.sh
+++ flask/start-gunicorn-generate-testcases.sh
@@ -28,6 +28,10 @@ gunicorn -w 4 -k flask_sockets.worker flaskr:app  -b '0.0.0.0:8000' --debug --lo
 pid=$!
 disown $pid
 echo $pid > .gunicorn.pid
+python DataStreaming.py&
+pid=$!
+disown $pid
+echo $pid > .datastreaming.pid
 
 
 
