7e2cb4ca57cb36ac741c241132a99c9b0c976b7d
ranganathanm <mranga@nist.gov>
2015-03-31 19:36:40 -0400

    Fix bug in logging.

--- flask/DataStreaming.py
+++ flask/DataStreaming.py
@@ -661,7 +661,7 @@ def startStreamingServer():
                 occupancySock.listen(10)
                 memCache.setSocketServerPort(p)
                 portAssigned = True
-                util.debugPrint( "DataStreaming: Bound to port "+p)
+                util.debugPrint( "DataStreaming: Bound to port "+ int(p))
                 break
             except:
                 print sys.exc_info()
