042e95f763d1000d9a916fa72321016b98ced13c
mranga <mranga@msodtest.nist.gov>
2015-07-10 18:59:19 -0400

    Run nginx as spectrumbrowser user.

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,3 +1,2 @@
-echo "Pem passphrase is ranga"
 mkdir -p /tmp/logs/nginx
-$NGINX_HOME/install/sbin/nginx -c $SPECTRUM_BROWSER_HOME/nginx/nginx.conf.test
+nginx -c $SPECTRUM_BROWSER_HOME/nginx/nginx.conf.test
