2f567c623fa23981dfce6d4c88df54e6894a9a41
klhicks <klhicks@users.noreply.github.com>
2015-07-17 11:27:06 -0600

    Update fabtargets.py

--- devel/deploy/fabtargets.py
+++ devel/deploy/fabtargets.py
@@ -68,40 +68,40 @@ def buildDatabase(): #build process for db server
 
 @roles('spectrumbrowser')
 def buildServer(): #build process for web server
-       sbHome = getSbHome()
-       sudo('rm -rf /var/log/flask')
-       sudo('rm -f /var/log/nginx/*')
-       sudo('rm -f /var/log/gunicorn/*')
-       sudo('rm -f /var/log/occupancy.log')
-       sudo('rm -f /var/log/streaming.log')
+    sbHome = getSbHome()
+    sudo('rm -rf /var/log/flask')
+    sudo('rm -f /var/log/nginx/*')
+    sudo('rm -f /var/log/gunicorn/*')
+    sudo('rm -f /var/log/occupancy.log')
+    sudo('rm -f /var/log/streaming.log')
 
-       with settings(warn_only=True):
-           sudo('adduser --system spectrumbrowser')
-           sudo('mkdir -p ' + sbHome)
-           sudo('chown -R spectrumbrowser ' + sbHome)
+    with settings(warn_only=True):
+        sudo('adduser --system spectrumbrowser')
+        sudo('mkdir -p ' + sbHome)
+        sudo('chown -R spectrumbrowser ' + sbHome)
 
-       put('/tmp/flask.tar.gz', '/tmp/flask.tar.gz')
-       put('/tmp/nginx.tar.gz', '/tmp/nginx.tar.gz')
-       put('/tmp/services.tar.gz', '/tmp/services.tar.gz')
-       sudo('tar -xvzf /tmp/flask.tar.gz -C ' + sbHome)
-       sudo('tar -xvzf /tmp/nginx.tar.gz -C ' + sbHome)
-       sudo('tar -xvzf /tmp/services.tar.gz -C ' + sbHome)
+    put('/tmp/flask.tar.gz', '/tmp/flask.tar.gz')
+    put('/tmp/nginx.tar.gz', '/tmp/nginx.tar.gz')
+    put('/tmp/services.tar.gz', '/tmp/services.tar.gz')
+    sudo('tar -xvzf /tmp/flask.tar.gz -C ' + sbHome)
+    sudo('tar -xvzf /tmp/nginx.tar.gz -C ' + sbHome)
+    sudo('tar -xvzf /tmp/services.tar.gz -C ' + sbHome)
 
-       put('nginx.repo', '/etc/yum.repos.d/nginx.repo', use_sudo=True)
-       put('MSODConfig.json.setup', sbHome + '/MSODConfig.json', use_sudo=True)
-       put('python_pip_requirements.txt', sbHome + '/python_pip_requirements.txt', use_sudo=True)
-       put('install_stack.sh', sbHome + '/install_stack.sh', use_sudo=True)
-       put('redhat_stack.txt', sbHome + '/redhat_stack.txt', use_sudo=True)
-       put('get-pip.py', sbHome + '/get-pip.py', use_sudo=True)
-       put('setup-config.py', sbHome + '/setup-config.py', use_sudo=True)
-       put('Config.gburg.txt', sbHome + '/Config.gburg.txt', use_sudo=True)
-       put(getProjectHome() + '/Makefile', sbHome + '/Makefile', use_sudo=True)
+    put('nginx.repo', '/etc/yum.repos.d/nginx.repo', use_sudo=True)
+    put('MSODConfig.json.setup', sbHome + '/MSODConfig.json', use_sudo=True)
+    put('python_pip_requirements.txt', sbHome + '/python_pip_requirements.txt', use_sudo=True)
+    put('install_stack.sh', sbHome + '/install_stack.sh', use_sudo=True)
+    put('redhat_stack.txt', sbHome + '/redhat_stack.txt', use_sudo=True)
+    put('get-pip.py', sbHome + '/get-pip.py', use_sudo=True)
+    put('setup-config.py', sbHome + '/setup-config.py', use_sudo=True)
+    put('Config.gburg.txt', sbHome + '/Config.gburg.txt', use_sudo=True)
+    put(getProjectHome() + '/Makefile', sbHome + '/Makefile', use_sudo=True)
 
-       dbrole = env.roledefs['database']
-       dbhost = dbrole['hosts']
+    dbrole = env.roledefs['database']
+    dbhost = dbrole['hosts']
 
-       with cd(sbHome):
-           sudo('sh install_stack.sh')
-           sudo('make REPO_HOME=' + sbHome + ' install')
-           sudo('python setup-config.py -host ' + dbhost)
+    with cd(sbHome):
+        sudo('sh install_stack.sh')
+        sudo('make REPO_HOME=' + sbHome + ' install')
+        sudo('python setup-config.py -host ' + dbhost)
 
