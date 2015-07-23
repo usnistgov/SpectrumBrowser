ae67dea3f81167eb6a2acb8247bd1bb333d758bf
M. Ranganathan <mranga@nist.gov>
2015-07-23 12:58:02 -0400

    Resource monitoring service (Admin) fixes. Also fixed up rounding in populate_db.py

--- devel/scripts/restart-msod.sh
+++ devel/scripts/restart-msod.sh
@@ -4,7 +4,8 @@ kill -9 $(cat .admin.pid)
 kill -INT $(cat .memcached.pid)
 kill -INT $(cat .streaming.pid)
 kill -INT $(cat .occupancy.pid)
-rm .gunicorn.pid .admin.pid .memcached.pid .streaming.pid .occupancy.pid
+kill -INT $(cat .monitoring.pid)
+rm .gunicorn.pid .admin.pid .memcached.pid .streaming.pid .occupancy.pid .monitoring.pid
 sleep 5
 source start-msod.sh
 
