5cc560f2711aaa5caeda63ef707e7af1c058b678
Matthew Berntson <mdb4@nist.gov>
2015-07-29 16:48:50 -0400

    Included the functions for Service Control

---
+++ flask/ServiceControlFunctions.py
@@ -0,0 +1,98 @@
+'''
+Created on Jul 28, 2015
+
+@author: mdb4
+'''
+import sys
+import traceback
+import util
+from Defines import SERVICE_NAMES
+import subprocess
+
+def thisServiceStatus(service):
+    try:
+        if service in SERVICE_NAMES:
+            output = subprocess.Popen(["/usr/bin/sudo","/sbin/service",service,"status"],stdout=subprocess.PIPE)
+            
+            statusRawInit, errorStr = output.communicate()
+            
+            if not errorStr == None:
+                util.debugPrint("Error String detected (status): " + str(errorStr))
+                return -1
+            
+            statusRaw = statusRawInit.split()
+            util.debugPrint("statusRaw: " + str(statusRaw))
+            
+            if "running" in statusRaw:
+                return 0
+            elif "stopped" in statusRaw:
+                return 1
+            else:
+                return -1
+            
+        else:
+            util.debugPrint(service + " does not match a service")
+            return -1
+
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        print sys.exc_info()
+        traceback.print_exc()
+        util.logStackTrace(sys.exc_info())
+        raise
+
+def stopThisService(service):
+    try:
+        if service in SERVICE_NAMES:
+            if service == SERVICE_NAMES[0]:
+                return False
+            else:
+                output = subprocess.Popen(["/usr/bin/sudo","/sbin/service",service,"stop"],stdout=subprocess.PIPE)
+                
+                stopRawInit, errorStr = output.communicate()
+            
+                if not errorStr == None:
+                    util.debugPrint("Error String detected (stop): " + str(errorStr))
+                    return False
+                
+                util.debugPrint("output.communicate() (stop): " + str(stopRawInit))
+                return True
+        else:
+            util.debugPrint(service + " does not match a service")
+            return False
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        print sys.exc_info()
+        traceback.print_exc()
+        util.logStackTrace(sys.exc_info())
+        raise
+
+def restartThisService(service):
+    try:
+        if stopThisService(service):
+            if service in SERVICE_NAMES:
+                if service == SERVICE_NAMES[0]:
+                    return False
+                else:
+                    output = subprocess.Popen(["/usr/bin/sudo","/sbin/service","admin","start"],stdout=subprocess.PIPE)
+                
+                    restartRawInit, errorStr = output.communicate()
+            
+                    if not errorStr == None:
+                        util.debugPrint("Error String detected (restart): " + str(errorStr))
+                        return False
+                
+                    util.debugPrint("output.communicate() (restart): " + str(restartRawInit))
+                    return True
+            else:
+                util.debugPrint(service + " does not match a service")
+                return False
+        else:
+            util.debugPrint(service + " was not properly restarted")
+            return False
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        print sys.exc_info()
+        traceback.print_exc()
+        util.logStackTrace(sys.exc_info())
+        raise
