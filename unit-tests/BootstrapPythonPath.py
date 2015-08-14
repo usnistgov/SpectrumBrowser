a77e31d3a1dca8fd0e787b2500c8cebfe4b9657f
M. Ranganathan <mranga@nist.gov>
2015-08-14 18:52:08 -0400

    Refactoring -- added missing files.

---
+++ unit-tests/BootstrapPythonPath.py
@@ -0,0 +1,22 @@
+import json as js
+import sys
+import subprocess
+def parse_msod_config():
+    configFile = open(getProjectHome() + "/.msod/MSODConfig.json")
+    msodConfig = js.load(configFile)
+    return msodConfig
+
+def getProjectHome(): #finds the default directory of installation
+    command = ['git', 'rev-parse', '--show-toplevel']
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    return out.strip()
+
+def getSbHome():
+    msodConfig = parse_msod_config()
+    return str(msodConfig["SPECTRUM_BROWSER_HOME"])
+
+def setPath():
+    msodConfig = parse_msod_config()
+    sbHome = str(msodConfig["SPECTRUM_BROWSER_HOME"])
+    sys.path.append(sbHome + "/services/common")
