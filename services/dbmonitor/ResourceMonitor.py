ca5c55601248cf59fcf365a05f994fd2af69f112
khicks <khicks@its.bldrdoc.gov>
2015-08-17 13:17:31 -0600

    dbmonitor update

--- services/dbmonitor/ResourceMonitor.py
+++ services/dbmonitor/ResourceMonitor.py
@@ -6,7 +6,77 @@ import time
 import argparse
 import signal
 from ReadDiskUtil import readDiskUtil
+from pymongo import MongoClient
 
+def readResourceUsage(dV):
+    timePerMeasurement = 0.2
+    timePerCapture = 1 # 1 sec per capture
+    measurementsPerCapture = timePerCapture/timePerMeasurement
+
+    try:
+        while True:
+
+            if not "firstTime" in vars():
+                firstTime = True
+            if not "netSentValuePrev" in vars():
+                netSentValuePrev = 0
+            if not "netRecvValuePrev" in vars():
+                netRecvValuePrev = 0
+
+            cpuData = 0
+            vmemData = 0
+            netSentData = 0
+            netRecvData = 0
+
+            bufferCounter = 0
+
+            while True:
+                cpu = psutil.cpu_percent()
+                vmem = psutil.virtual_memory()._asdict()['percent']
+
+
+                hostName = Config.getHostName()
+                monitoredInterface = None
+                try:
+                    if hostName != "UNKNOWN":
+                        ipAddress = socket.gethostbyname(hostName)
+                        for interface in netifaces.interfaces():
+                            if netifaces.AF_INET in netifaces.ifaddresses(interface):
+                                for link in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
+                                    if link['addr'] == ipAddress:
+                                        monitoredInterface = interface
+                                        break
+                except:
+                    util.errorPrint("Could not resolve hostname " + hostName)
+
+                if monitoredInterface != None:
+                    netSent = psutil.net_io_counters(pernic=True)[monitoredInterface]._asdict()['bytes_sent']
+                    netRecv = psutil.net_io_counters(pernic=True)[monitoredInterface]._asdict()['bytes_recv']
+                else:
+                    netSent = 0
+                    netRecv = 0
+
+
+                cpuData = cpuData + cpu
+                vmemData = vmemData + vmem
+                netSentData = netSentData + netSent
+                netRecvData = netRecvData + netRecv
+
+                client = MongoClient('localhost',27017);
+                db = client['systemResources']
+                collection = db['dbResources']
+                collection.insert({'DISK': dV})
+
+
+                sleepTime = timePerMeasurement
+                time.sleep(sleepTime)
+
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        print sys.exc_info()
+        import traceback
+        traceback.print_exc()
+        util.logStackTrace(sys.exc_info())
 
 
 if __name__ == '__main__':
@@ -18,6 +88,7 @@ if __name__ == '__main__':
 
     with util.PidFile(args.pidfile):
         Log.configureLogging("dbmonitor")
-        disk = readDiskUtil(args.dbpath)
-        time.sleep(60)
+        diskVal = readDiskUtil(args.dbpath)
+        time.sleep(30)
+        readResourceUsage(diskVal)
         
