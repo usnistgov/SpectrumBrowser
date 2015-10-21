b2a00cb5187aca29e965887a4330178a2f2e15fa
M. Ranganathan <mranga@nist.gov>
2015-10-21 17:44:01 -0400

    Fix in aide running script.

--- aide/runaide.sh
+++ aide/runaide.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 FILENAME=/tmp/aide.out
-nice -19 aide --check > $FILENAME
+nice -19 /usr/sbin/aide --check > $FILENAME
 FILESIZE=$(stat -c%s "$FILENAME")
 if [ $FILESIZE == 0 ] ; then
+   echo /bin/date >> /var/log/aide.out
    echo "AIDE: Security scan found no violations" >> /var/log/aide.out
 elif grep -F "### All files match AIDE database. Looks okay" /tmp/aide.out ; then
+   echo /bin/date >> /var/log/aide.out
    echo "AIDE: Security scan found no violations" >> /var/log/aide.out
 else
     cat /tmp/aide.out | /opt/SpectrumBrowser/swaks -4  --to mranga@nist.gov --from mranga@nist.gov --server smtp.nist.gov --h-Subject "MSOD: aide Scan Report" --body -
