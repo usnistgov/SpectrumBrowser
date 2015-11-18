adc70c0d170a5fe63008f7b62b100734ad5b3bb6
M. Ranganathan <mranga@nist.gov>
2015-11-18 13:15:23 -0500

    Added test cases.

---
+++ unit-tests/test-get-sensor-config.py
@@ -0,0 +1,41 @@
+import unittest
+import json
+import requests
+import argparse
+
+class  SensorConfigTest(unittest.TestCase):
+    def setUp(self):
+        global host
+        global webPort
+        self.sensorId = "E6R16W5XS"
+	self.serverUrlPrefix = "https://" + host + ":" + str(webPort)
+
+    def test_get_sensor_config(self):
+        r = requests.post(self.serverUrlPrefix + "/sensordb/getSensorConfig/" + self.sensorId,verify=False)
+        jsonVal = r.json()
+        print json.dumps(jsonVal, indent=4)
+        self.assertTrue(json != None)
+        self.assertTrue(jsonVal["sensorConfig"]["SensorID"] == self.sensorId)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process command line args")
+    parser.add_argument("-host",help="Server host.")
+    parser.add_argument("-port",help="Server port.")
+    args = parser.parse_args()
+    global host
+    global webPort
+    host = args.host
+    if host == None:
+        host = os.environ.get("MSOD_WEB_HOST")
+    webPort = args.port
+    if webPort == None:
+       webPort = "443"
+
+    if host == None or webPort == None:
+        print "Require host and web port"
+    webPortInt = int(webPort)
+    if webPortInt < 0 :
+        print "Invalid params"
+        os._exit()
+    suite = unittest.TestLoader().loadTestsFromTestCase(SensorConfigTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
