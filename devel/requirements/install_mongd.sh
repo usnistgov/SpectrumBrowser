c2e1348037b4a8a1870327eab9d75739422b450f
Naceur El Ouni <N-ELOUNI@users.noreply.github.com>
2016-07-25 17:02:40 -0400

    Update install_mongd.sh

--- devel/requirements/install_mongd.sh
+++ devel/requirements/install_mongd.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
 echo
 echo "============== Installing mongd =============="
 mkdir -p /opt/mongodb-download
 mkdir -p /opt/mongodb
-wget https://www.mongodb.org/dr/fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.10.tgz/download -O /opt/mongodb-download/download
+wget https://www.mongodb.org/dr/fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.10.tgz/download 
+#-O /opt/mongodb-download/download
 tar -xvzf /opt/mongodb-download/download -C /opt/
